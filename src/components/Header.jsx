a6a73b7a2c23d75e5f055eb0f599b46800b15d8c
Emmanuel Nsikan-David <en56434@gmail.com>
2023-03-25 14:19:18 +0100

    CSS and light mode update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import moon from "../assets/icon-moon.svg"
+import sun from "../assets/icon-sun.svg"
 
 
 function Header() {
+  const [icon, setIcon] = useState(moon)
+  function changeTheme(){
+    setIcon(()=> icon == moon? sun : moon)
+    document.body.classList.contains("light") ? document.body.classList = "dark" : document.body.classList = "light"
+  }
   return (
     <div className="header">
     <h1 className="logo">TODO</h1>
 
-    <div className="theme">
-      <img src={moon} alt="" />
+    <div 
+    onClick={changeTheme}
+    className="theme">
+      <img src={icon} alt="" />
     </div>
   </div>
   )
