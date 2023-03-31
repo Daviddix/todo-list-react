fe48a491a458fcb30d0498d33ac858662fa11c46
Emmanuel Nsikan-David <en56434@gmail.com>
2023-04-01 00:39:32 +0100

    added theme switch to local storage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react"
+import { useLayoutEffect, useState } from "react"
 import moon from "../assets/icon-moon.svg"
 import sun from "../assets/icon-sun.svg"
 
 
 function Header() {
-  const [icon, setIcon] = useState(sun)
+  const [icon, setIcon] = useState(()=> localStorage.getItem("todo-list-theme") == "light"? moon : sun || sun)
+  const [theme, setTheme] = useState(()=> localStorage.getItem("todo-list-theme") || "dark")
+
+  useLayoutEffect(()=>{
+    if (theme == "dak") {
+      document.body.classList = "dark"
+    }else if(theme == "light"){
+      document.body.classList = "light"
+    }
+  }, [theme])
+
   function changeTheme(){
     setIcon(()=> icon == moon? sun : moon)
+    localStorage.setItem("todo-list-theme", icon == sun? "light" : "dark")
     document.body.classList.contains("light") ? document.body.classList = "dark" : document.body.classList = "light"
   }
   return (
